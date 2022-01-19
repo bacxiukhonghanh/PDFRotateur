83f1088175513843409b79a43831736f0d5663ef
bacxiukhonghanh <markrobinsonsecurity@gmail.com>
2022-01-19 22:28:26 +0700

    Added the project's files

---
+++ PDFRotateur/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿
+namespace PDFRotateur
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pdfViewer1 = new PdfiumViewer.PdfViewer();
+            this.pdfViewer2 = new PdfiumViewer.PdfViewer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choisissez un fichier PDF pour faire pivoter";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(272, 64);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OUVRIR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(640, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(217, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Choisissez un degré de rotation";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0",
+            "90",
+            "180",
+            "270"});
+            this.comboBox1.Location = new System.Drawing.Point(863, 65);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(118, 26);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(1122, 55);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(140, 42);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "ENREGISTRER SOUS";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Fichier PDF (*.pdf)|*.pdf";
+            this.openFileDialog1.Title = "Choisir un fichier PDF";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(987, 65);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(129, 26);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Faire pivoter";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Fichier PDF (*.pdf)|*.pdf";
+            this.saveFileDialog1.Title = "Enregistrer un fichier PDF";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(513, 9);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 28);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Tiếng Việt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(606, 9);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(82, 28);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "English";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(692, 9);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(90, 28);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "Français";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(1013, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(210, 18);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "from Bạc Xỉu Không Hành with";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1222, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 41);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pdfViewer1
+            // 
+            this.pdfViewer1.BackColor = System.Drawing.Color.Black;
+            this.pdfViewer1.Location = new System.Drawing.Point(0, 103);
+            this.pdfViewer1.Name = "pdfViewer1";
+            this.pdfViewer1.Size = new System.Drawing.Size(630, 578);
+            this.pdfViewer1.TabIndex = 17;
+            this.pdfViewer1.ZoomMode = PdfiumViewer.PdfViewerZoomMode.FitBest;
+            // 
+            // pdfViewer2
+            // 
+            this.pdfViewer2.BackColor = System.Drawing.Color.Black;
+            this.pdfViewer2.Location = new System.Drawing.Point(632, 103);
+            this.pdfViewer2.Name = "pdfViewer2";
+            this.pdfViewer2.Size = new System.Drawing.Size(630, 578);
+            this.pdfViewer2.TabIndex = 18;
+            this.pdfViewer2.ZoomMode = PdfiumViewer.PdfViewerZoomMode.FitBest;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(358, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(258, 21);
+            this.textBox1.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pdfViewer2);
+            this.Controls.Add(this.pdfViewer1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Programme de rotation des fichiers PDF";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private PdfiumViewer.PdfViewer pdfViewer1;
+        private PdfiumViewer.PdfViewer pdfViewer2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ PDFRotateur/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using iTextSharp.text.pdf;
+
+namespace PDFRotateur
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button6_Click(sender, e);
+        }
+
+        public static string GetTimestamp(DateTime value)
+        {
+            return value.ToString("yyyyMMddHHmmssffff");
+        }
+
+        string rotationOutputFilePath = Application.StartupPath;
+        string messageBoxText = "";
+        string messageBoxTitle = "";
+        MemoryStream outputFileStream;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    var filePath = openFileDialog1.FileName;
+                    textBox1.Text = filePath;
+                    pdfViewer1.Document = PdfiumViewer.PdfDocument.Load(filePath);
+                }
+            }
+            catch (Exception e_)
+            {
+                MessageBox.Show(e_.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text.Trim() != "" && comboBox1.Text.Trim() != "")
+                {
+                    if (outputFileStream != null) outputFileStream.Close();
+                    string timestamp = GetTimestamp(DateTime.Now);
+                    string outputFilePath = Path.Combine(rotationOutputFilePath, timestamp + ".pdf");
+
+                    PdfReader reader = new PdfReader(textBox1.Text);
+                    int pagesCount = reader.NumberOfPages;
+
+                    for (int n = 1; n <= pagesCount; n++)
+                    {
+                        PdfDictionary page = reader.GetPageN(n);
+                        PdfNumber rotate = page.GetAsNumber(PdfName.ROTATE);
+                        int rotation = Convert.ToInt32(comboBox1.Text);
+
+                        page.Put(PdfName.ROTATE, new PdfNumber(rotation));
+                    }
+
+                    FileStream fs = File.OpenWrite(outputFilePath);
+                    PdfStamper stamper = new PdfStamper(reader, fs);
+                    stamper.Close();
+                    reader.Close();
+
+                    outputFileStream = new MemoryStream(File.ReadAllBytes(outputFilePath));
+                    pdfViewer2.Document = PdfiumViewer.PdfDocument.Load(outputFileStream);
+                    File.Delete(outputFilePath);
+
+                    //pdfViewer2.Document = null;
+                    //pdfViewer2.Document = PdfiumViewer.PdfDocument.Load(textBox1.Text);
+                    //int rotation = Convert.ToInt32(comboBox1.Text);
+                    //for (int i = 0; i < pdfViewer1.Document.PageCount; i++)
+                    //{
+                    //    switch (rotation)
+                    //    {
+                    //        case 0:
+                    //            pdfViewer2.Document.RotatePage(i, PdfiumViewer.PdfRotation.Rotate0);
+                    //            break;
+                    //        case 90:
+                    //            pdfViewer2.Document.RotatePage(i, PdfiumViewer.PdfRotation.Rotate90);
+                    //            break;
+                    //        case 180:
+                    //            pdfViewer2.Document.RotatePage(i, PdfiumViewer.PdfRotation.Rotate180);
+                    //            break;
+                    //        case 270:
+                    //            pdfViewer2.Document.RotatePage(i, PdfiumViewer.PdfRotation.Rotate270);
+                    //            break;
+                    //    }
+                    //}
+                }
+            }
+            catch (Exception e_)
+            {
+                MessageBox.Show(e_.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                saveFileDialog1.ShowDialog();
+                if (saveFileDialog1.FileName != "")
+                {
+                    pdfViewer2.Document.Save(saveFileDialog1.FileName);
+                    MessageBox.Show(messageBoxText, messageBoxTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception e_)
+            {
+                MessageBox.Show(e_.Message);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Choose a PDF file to rotate";
+            button1.Text = "BROWSE";
+            label3.Text = "Choose a rotation degree";
+            button4.Text = "Rotate";
+            button3.Text = "SAVE AS";
+            this.Text = "PDF files rotation tool";
+            openFileDialog1.Filter = "PDF files (*.pdf)|*.pdf";
+            openFileDialog1.Title = "Choose a PDF file to rotate";
+            saveFileDialog1.Filter = "PDF files (*.pdf)|*.pdf";
+            saveFileDialog1.Title = "Save the PDF file";
+            messageBoxText = "The operation completed successfully";
+            messageBoxTitle = "Completed";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Choisissez un fichier PDF pour faire pivoter";
+            button1.Text = "OUVRIR";
+            label3.Text = "Choisissez un degré de rotation";
+            button4.Text = "Faire pivoter";
+            button3.Text = "ENREGISTRER SOUS";
+            this.Text = "Programme de rotation des fichiers PDF";
+            openFileDialog1.Filter = "Fichier PDF (*.pdf)|*.pdf";
+            openFileDialog1.Title = "Choisir un fichier PDF";
+            saveFileDialog1.Filter = "Fichier PDF (*.pdf)|*.pdf";
+            saveFileDialog1.Title = "Enregistrer un fichier PDF";
+            messageBoxText = "L'opération s'est terminée avec succès";
+            messageBoxTitle = "Terminée";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Chọn 1 tập tin PDF để xoay";
+            button1.Text = "MỞ";
+            label3.Text = "Chọn độ xoay";
+            button4.Text = "Xoay";
+            button3.Text = "LƯU DƯỚI DẠNG";
+            this.Text = "Công cụ xoay tập tin PDF";
+            openFileDialog1.Filter = "Tập tin PDF (*.pdf)|*.pdf";
+            openFileDialog1.Title = "Chọn 1 tập tin PDF để xoay";
+            saveFileDialog1.Filter = "Tập tin PDF (*.pdf)|*.pdf";
+            saveFileDialog1.Title = "Lưu tập tin PDF";
+            messageBoxText = "Tác vụ hoàn tất";
+            messageBoxTitle = "Hoàn thành";
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                if (outputFileStream != null) outputFileStream.Close();
+            }
+            catch (Exception e_)
+            {
+                //MessageBox.Show(e_.Message);
+            }
+        }
+    }
+}
